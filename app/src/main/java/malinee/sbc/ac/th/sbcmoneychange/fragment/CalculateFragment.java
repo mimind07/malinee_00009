2c92cccaec1dda7af1cd09d43a6228b440682708
mind <mind4minute@gmail.com>
2018-01-13 11:50:50 +0700

    Create Toolbar success

--- app/src/main/java/malinee/sbc/ac/th/sbcmoneychange/fragment/CalculateFragment.java
+++ app/src/main/java/malinee/sbc/ac/th/sbcmoneychange/fragment/CalculateFragment.java
@@ -3,10 +3,14 @@ package malinee.sbc.ac.th.sbcmoneychange.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.concurrent.Exchanger;
+
+import malinee.sbc.ac.th.sbcmoneychange.MainActivity;
 import malinee.sbc.ac.th.sbcmoneychange.R;
 
 /**
@@ -15,6 +19,28 @@ import malinee.sbc.ac.th.sbcmoneychange.R;
 
 public class CalculateFragment extends Fragment{
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        //    Toolbar Controller
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.th_exchange));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.th_sub_exchange));
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }   //Main Method
 
     @Nullable
     @Override
