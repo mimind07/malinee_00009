74cc1c803edc1e2cbbf90e926a79152d4d2fefab
mind <mind4minute@gmail.com>
2018-01-13 10:47:14 +0700

    Replace Fragment Success

---
+++ app/src/main/java/malinee/sbc/ac/th/sbcmoneychange/fragment/CalculateFragment.java
@@ -0,0 +1,27 @@
+package malinee.sbc.ac.th.sbcmoneychange.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import malinee.sbc.ac.th.sbcmoneychange.R;
+
+/**
+ * Created by LAB3201_00 on 13/1/2561.
+ */
+
+public class CalculateFragment extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calculate,container,false);
+
+        return view;
+
+    }
+} //Main Class
--- app/src/main/java/malinee/sbc/ac/th/sbcmoneychange/fragment/MainFragment.java
+++ app/src/main/java/malinee/sbc/ac/th/sbcmoneychange/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import malinee.sbc.ac.th.sbcmoneychange.R;
 
@@ -14,6 +15,33 @@ import malinee.sbc.ac.th.sbcmoneychange.R;
  */
 
 public class MainFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+        //    Calculate Controller
+        Button button = getView().findViewById(R.id.btnCalculate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            //    Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new CalculateFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+
+    }   //Main Method
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
